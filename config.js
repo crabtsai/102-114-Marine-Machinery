386edf35f5c393e46e1d98b5774af406e18bd791
crabtsai <87925842+crabtsai@users.noreply.github.com>
2026-07-10 17:25:08 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,25 @@
+window.APP_CONFIG = {
+  googleSheet: {
+    sheetId: "1gQM_dJQLHQWz82vJUHYgbILQUtGwjITZuB6AO8pWGA4",
+
+    apiKey: "",
+
+    // 關閉自動搜尋分頁
+    autoDiscoverSubjects: false,
+
+    defaultSubjectId: "1294771581",
+
+    subjects: [
+      {
+        id: "1294771581",
+        name: "自然",
+        sheetName: "自然"
+      },
+      {
+        id: "1713008525",
+        name: "社會",
+        sheetName: "社會"
+      }
+    ]
+  }
+};
---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Web App 公開設定。
+// 這不是管理員私鑰；仍需搭配正確的 Authentication 與 Firestore Rules。
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCTmXQR3XkLQ42DY1ALSeVhTlacUV8PNfs",
+  authDomain: "multisheetmockexamsystem.firebaseapp.com",
+  projectId: "multisheetmockexamsystem",
+  storageBucket: "multisheetmockexamsystem.firebasestorage.app",
+  messagingSenderId: "127284371306",
+  appId: "1:127284371306:web:188d100607ca59ed728f13",
+  measurementId: "G-MCTPQF393W"
+};
